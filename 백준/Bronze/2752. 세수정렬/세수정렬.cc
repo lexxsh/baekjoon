a8fcb56190decc56a717ba347a264251b2976ea4
lexxsh <110239629+lexxsh@users.noreply.github.com>
2022-08-14 18:55:28 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a[4];
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a, a + 3);
+    for (int i = 0; i < 3; i++)cout << a[i] << ' ';
+}
