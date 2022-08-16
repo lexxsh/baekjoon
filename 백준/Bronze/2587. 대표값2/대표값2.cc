42fbce3ee37538ab9faf5858c2e34a79bec46e89
lexxsh <110239629+lexxsh@users.noreply.github.com>
2022-08-16 12:45:19 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int a[5],sum(0);
+  for(int i = 0;i<5;i++){
+    cin>>a[i];
+    sum+=a[i];
+  }
+  sort(a,a+5);
+  cout << sum/5 <<"\n" << a[2];
+}
