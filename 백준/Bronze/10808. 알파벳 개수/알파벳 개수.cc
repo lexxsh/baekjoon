6039d9ccb18a389299e26bd4cd837177682cf86b
lexxsh <110239629+lexxsh@users.noreply.github.com>
2022-08-18 19:31:06 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,15 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+  int ans[26];
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
   string s;
-  int ans[26]={};
   cin>>s;
-  for(int i = 0;i<s.size();i++){
-    ans[s[i]-'a']++;
-  }
-  for(int e : ans){
-    cout<<e<<' ';
-  }
+  for(auto e : s) ans[e-'a']++;
+  for(auto e : ans) cout<<e<<' ';
 }
