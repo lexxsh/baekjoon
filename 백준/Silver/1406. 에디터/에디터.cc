dd5f678a9c1e18b4940750b2945dd1aba250e5b6
lexxsh <110239629+lexxsh@users.noreply.github.com>
2022-08-19 21:04:45 +0900

    [Silver II] Title: 에디터, Time: 72 ms, Memory: 21020 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  string m;
+  cin>>m;
+  list<char> l;
+  for(auto c : m) l.push_back(c); 
+  auto cuser = l.end();
+  int k;
+  cin>>k;
+  while(k--){
+    char order;
+    cin>>order;
+    if(order == 'P') {
+      char a;
+      cin>>a;
+      l.insert(cuser,a);
+    }
+    else if(order == 'L') {
+      if(cuser != l.begin()) cuser--;
+    }
+    else if(order == 'D') {
+      if(cuser !=l.end()) cuser++;
+    }
+    else {
+      if(cuser !=l.begin()){
+      cuser--;
+      cuser = l.erase(cuser);
+      }
+    }
+  }
+  for(auto c : l) cout<<c;
+}
