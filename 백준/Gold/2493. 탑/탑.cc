337a1637b578a534ca01279cf5d60a027ba79b78
lexxsh <110239629+lexxsh@users.noreply.github.com>
2022-08-25 19:02:01 +0900

    [Gold V] Title: 탑, Time: 100 ms, Memory: 6300 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.cc
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    stack<pair<int, int> > s;
+    int num, height;
+    cin >> num;
+    for(int i = 1;i<=num;i++) {
+        cin >> height;
+        while (!s.empty()) {
+            if (height < s.top().second) {
+                cout << s.top().first << " ";
+                break;
+            }
+            s.pop();
+        }
+        if (s.empty()) {
+            cout << 0 << " ";
+            }
+        s.push(make_pair(i,height));
+        }
+}
