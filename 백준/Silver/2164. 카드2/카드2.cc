3ce2161d614b14a01d50fa4b588f9d24c21035a5
lexxsh <110239629+lexxsh@users.noreply.github.com>
2022-08-30 16:08:19 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,ans;
+    cin >> n;
+    queue<int> q;
+    for (int i = 1; i <= n; i++) {
+        q.push(i);
+    }
+    while (q.size()!=1) {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front();
+}
