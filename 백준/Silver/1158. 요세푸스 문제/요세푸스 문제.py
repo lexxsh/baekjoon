60ea91bfac0bd77038eae669cce138d876010481
이상혁 Lee Sang hyeok <110239629+lexxsh@users.noreply.github.com>
2024-09-09 16:44:54 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,14 +1,8 @@
-n, k = map(int,input().split())
-index = 0
-arr = list(range(1,n+1))
-result = []
-
-while len(arr) != 0:
-    index += (k-1)
-    index = index % len(arr)
-    result.append(arr.pop(index))
-print("<", end='')
-for i in range(n-1):
-    print(result[i],end=", ")
-print(result[n-1], end="")
-print(">", end="")
+n,k=map(int,input().split())
+a=list(range(1,n+1))
+r=[]
+i=0
+while a:
+ i=(i+k-1)%len(a)
+ r.append(a.pop(i))
+print('<'+', '.join(map(str,r))+'>')
