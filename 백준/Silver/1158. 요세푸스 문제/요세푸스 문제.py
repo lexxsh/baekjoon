0a6f46038080b01f11b8d08fd4b22fb76ec56b53
이상혁 Lee Sang hyeok <110239629+lexxsh@users.noreply.github.com>
2024-09-09 16:54:11 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,1 +1,4 @@
-n,k=map(int,input().split());l=list(range(1,n+1));j=0;print('<'+', '.join(str(l.pop(j:=(j+k-1)%len(l)))for _ in'_'*n)+'>')
+n,k=map(int,input().split())
+l=list(range(1,n+1))
+j=0
+print('<'+', '.join(str(l.pop(j:=(j+k-1)%len(l)))for _ in'_'*n)+'>')
