002834a692603717c968d7f2004299138d566dcd
이상혁 Lee Sang hyeok <110239629+lexxsh@users.noreply.github.com>
2025-01-09 21:54:56 +0900

    [Silver II] Title: 최대 힙, Time: 132 ms, Memory: 48580 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,14 @@
+import sys,heapq
+
+input = sys.stdin.readline
+n = int(input())
+heap = []
+for i in range(n):
+    num = int(input())
+    if num == 0:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap,(-num,num))
