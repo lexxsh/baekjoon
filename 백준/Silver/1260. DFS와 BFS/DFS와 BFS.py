d0083fc19eb9ebb024966a8eeaa4b8f4c1f36073
이상혁 Lee Sang hyeok <110239629+lexxsh@users.noreply.github.com>
2025-01-13 00:17:07 +0900

    [Silver II] Title: DFS와 BFS, Time: 172 ms, Memory: 35652 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,6 +1,5 @@
 import sys
-from collections import defaultdict
-
+from collections import defaultdict, deque
 def DFS(graph, start):
     visit = list()
     stack = list()
@@ -13,7 +12,7 @@ def DFS(graph, start):
     print(*visit,sep=" ")
     return visit
     
-def BFS(graph,start):
+def BFS_list(graph,start):
     visit = list()
     queue = list()
     queue.append(start)
@@ -25,6 +24,31 @@ def BFS(graph,start):
     print(*visit,sep=" ")
     return visit
 
+def BFS_deque(graph,start):
+    visit = set()
+    queue = deque()
+    queue.append(start)
+    while queue:
+        node = queue.popleft()
+        if node not in visit:
+            visit.add(node)
+            queue.extend(graph[node])
+    print(*visit,sep=" ")
+    return visit
+
+def BFS_deque_2(graph,start):
+    visit = [False] * (n + 1)
+    queue = deque()
+    queue.append(start)
+    ans = list()
+    while queue:
+        node = queue.popleft()
+        if not visit[node]:
+            visit[node] = True
+            ans.append(node)
+            queue.extend(graph[node])
+    print(*ans,sep=" ")
+    return visit
 
 input = sys.stdin.readline
 n,m,v = map(int,input().split())
@@ -37,4 +61,6 @@ for i in range(m):
 for key in G:
     G[key].sort()
 DFS(G,v)
-BFS(G,v)
+# BFS_list(G,v)
+# BFS_deque(G,v)
+BFS_deque_2(G,v)
