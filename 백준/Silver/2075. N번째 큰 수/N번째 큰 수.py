34cd51c5ed3676425e6049201b3b7e321302bdb5
이상혁 Lee Sang hyeok <110239629+lexxsh@users.noreply.github.com>
2025-03-10 20:02:09 +0900

    [Silver III] Title: N번째 큰 수, Time: 684 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -1,9 +1,14 @@
-import sys
-input = sys.stdin.readline
-
+import heapq
 n = int(input())
-answer = []
-for _ in range(n):
-    a = list(map(int,input().split()))
-    answer = sorted(a+answer,reverse=True)[:n]
-print(answer[-1])
+hq= []
+for i in range(n):
+    num = list(map(int,input().split()))
+    if i == 0:
+        for n in num:
+            heapq.heappush(hq,n)
+    else:
+        for n in num:
+            if n > hq[0]:
+                heapq.heappop(hq)
+                heapq.heappush(hq,n)
+print(heapq.heappop(hq))
